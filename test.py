65f3b53eff7a806ba17c44e379fd5bda1647060c
nakayama-ginga <ginga.akb@gmail.com>
2022-12-14 12:20:22 +0900

    マップ上にtweetを表示

---
+++ test.py
@@ -0,0 +1,7 @@
+
+array_lola1 = [[0,0,0,[0 for l in range(5)]] for j in range(2)]
+
+print(array_lola1[1][3][2])
+print(array_lola1[0][1])
+#array_lola = [[0 for i in range(3),[0 for l in range(5)]] for j in range(36)]
+#print(array_lola) # [longtitude,latitude,emotion]　のlat_num*lat_numの二次元配列
